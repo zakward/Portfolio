2c9e52a8e3588e8fd9b0933761dc6bc2c5c7fb46
Zak Ward <zakward85@gmail.com>
2022-03-04 19:27:14 -0600

    code push

---
+++ src/ProjectData.js
@@ -0,0 +1,22 @@
+
+
+export default [
+    {
+        title: "Task Organizer App",
+        description: "Full Stack Web Application that allows a user to track completed or incomplete tasks for work, home, and school.  Saves tasks to a database with full CRUD functionality",
+        imgUrl: "https://lh3.googleusercontent.com/noCaZSFd-LcjoNC4JK4-SaEAkpB1Vg-K1zQIK94IPoVJiTHzWshOB4-PIDJTSM1Y-E9M_w4gVxHJNJiJ05chIe-R5tRXh7SQVRH_UrhDQZEeIStUT1FxQca0xyvFXRXB7GdX3e15tXRWamCIapT69ubrSp3O83V4FrAPwCP_lrTVyy9TYFd1tYI6dy3OwTnxZtLa2dQcDRLd-_IuqbTeCbrX-0QS5cd3L_7XOlwhKQCpZWLGGM15rqqpGOdYqJttQ4TAJM_g9PpofIv_ZmN1tu7W20tuA5vRKbFrGZzX1u58PqQy5mIYqWSjqshva5iKqZnCzVpC9v2IiqXlSV8hccc6MHLSE1XwD_EWcem90qumMj6-jRMHIyi_G4zxEftj2s43KmIJ_k6CO2japyG6rt8jUR5t4sPOncY4DylWhj1zm602k1Z-G-Qj04JUbCvgVuFBcGNNeBK7Yk6vzblFV93eaBNq2mzp932rwOjYQdPEoa--VjnbrOCD8mYmHq5ONXZm3ZzaVgvb7npG_ehsc9WNmBnHexepwfdom8vyfyKYYjrwpr660AIggjeOQPFz_zUcGKxShKPWWy3TWuVYUkju129FxPFdAaTOXNXTNYiLFJjs9n7mzw9g-r2Oz9JHi_M4DV0cfnibzQoQXR-_h7Tud4-Fyd0mwQUKMWNt7YdLX5gFw496ONaYcje3Gq5PApyiPuLwNgIdrd9PhhEPSzW5kw=w1902-h937-no?authuser=0",
+        codeLink: "https://github.com/Level-5-Capstone-Amber-Zak/task-organizer-app"
+    },
+    {
+        title: "Cocktail Connoisseur",
+        description: "React App using Hooks to generate random cocktails or search by name or recipe from an API",
+        imgUrl: "https://lh3.googleusercontent.com/A0NRv8nS-lxzaKHW7W2ZPQEI4qPuIvtX3N6b2tcei2VaBx93wf3nk7JEbjw3mSsDEwFTct_wZJuBY64lqtu4phg0cOGuBN_i6VOZEaCIjhd_aDGJcER-N_DY1cONVrHnSGGuQrkgLT5Yj8CHqPdqhdHQJR1IvxdKfkVRRi0MRFhtJUeopzc0on_y9lTsiSbvJGA7AdWxjISRrN4aNY9P0F5z83NHhgVADBMkgobVXQkvnY9thDLp6ShsdEzewUjN7VniIAekLz-JP7upgKF6eIEeqx8UPDASt0V31pYNxj6Z2tZlFeaLc-BRLOTNS4zsvm-C2zkRVVN0qTv2PXbHMDMt-rt-CmURUTF8IPgOjqppJnyyb4GyJTV9pwL2Say6s6etUxVtC8eU3Gmv6y5YhGkvOsgSBjUFr8qVCKbHRS4AATOaC2BsZoGutVFTVq83V2q5Lt8uHvn8LnsT3zlVEF-UumKHZuivUwp1HI_o-cEHSiTsWy-wMhdqPXk58Wb95OPeNzKtg13N0pB7MeDtg1GYSoEOo3x6lc30VriFiWdzx41uM1jPWkckvQEizpZEDkAlKrdyuush5LCAYufTzqIkpgfIJCuTM00Ou5qH0wt865ggEei_DXA-dsG6q4Kp9LlcPc35X0GRXfKwjOUP5-Q-YxpYHrc8OgXwaqYiaqc1l29FK9pADy2xHXddJ9nerMxs8-aVpHqDglkXbbPAsLgcZw=w1887-h924-no?authuser=0",
+        codeLink: "https://github.com/zakward/VSchool/tree/main/assignments/Level%204/level-4-capstone-app"
+    },
+    {
+        title: "Marios Pest Control",
+        description: "Static website.  Showcases CSS styling and functionality",
+        imgUrl : "https://lh3.googleusercontent.com/sGbWCv23Pld0ye992SGoVK_HgSUSI5pMfUBevQKrdbkdbdXLkwnmiIyHJ6a-oGsQetuYnKng3ez9rMTpnRnhie2sUPT0Zdo4CdeMDR26wlcjtK7E72UE_qZhVWNAxl6ysRd6XQnRJfRd9vQPT0EIYFpLgQGsHH0YUNjhULskxp8TmL8ddxm1fIRtvtiJwNeZ40rWtYmX3ncf2qqkyRqP3SRYqX8DpbACsBKmv9WxLTnrxyiRdsF8pnobkpSx3AeRdcxMxrVCAcJeWjIlq21JsRtb2EFnJP75ONATENxqrP4JvSDM3GipoK6umgQ5HRGp1Ft4_INgPqaopFaFzhItnIjFn2QbsLOxCSUyhKt12PnqB84GipeLTo6eNjanVc_DANnaxgbBxwDcBTuzmRCmLCxxVZI8S8u7cB0Lbr2FnQpL2Dss1OM-MSm_JNLYZIaaY1aMSZ2FPrN8jA3HyYKSVZ_wzUZggaOnT0dS3ye3vDkHqkFV0MYErIzwPCdqXlAs4iiIkkToVDK10s8onJdw-TjNT-dXFs3rpSuBbH7E2gjwLh0SEChnDmlNdixWHbYXD0hhE8tfgktiQfOxidalcXvuaLBoVw8iw0POCEoCh2vlP_lBBqJpa3cUEs3Vx4oerT00ShWSHV3eSMg2DeszLphR67FuQcr_m8ar4GdJvZ1tZOG2XaXyp1dG90HUTs1gYDRqM8SDamYBdYf2TUhprMU5SA=w1920-h936-no?authuser=0",
+        codeLink: "https://github.com/zakward/VSchool/tree/main/assignments/Level%202/marios-pest-control"
+    }
+]
