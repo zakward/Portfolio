a01e5043a19a408b8d6ea7673709777a7fb729b9
Zak Ward <zakward85@gmail.com>
2022-03-24 17:59:29 -0500

    text change

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -19,7 +19,7 @@ function Biography() {
                 </div>
                 <div className = "biography-container">
                     <h1 className = "biography-title">Biography</h1>
-                    <p className = "biography">Hi, I’m Zak Ward, I am Web Development Instructor at VSchool.  I maintain a Department of Defense Top Secret Clearance.  I have a Bachelor’s Degree in Communications from the University of Central Arkansas.</p>
+                    <p className = "biography">Hi, I’m Zak Ward, I am a Web Development Instructor at VSchool.  I maintain a Department of Defense Top Secret Clearance.  I have a Bachelor’s Degree in Communications from the University of Central Arkansas.</p>
                     <p className = "biography">Previously, I worked as a Signal Officer in the United States Army, where I served as advisor to the Battalion Commander on all matters concerning communications, information technology, information security, and network management.  Aside from work, I am a conundrum of a man who has a passion for technology and web development as well as a love for the great outdoors.</p>   
                     <p className = "biography">My greatest passion is helping others.  Through my time as a professional firefighter, Army officer, and business-owner, I developed and honed in on my talents of leadership, loyalty, and teamwork.  Web Development allows me to use my high attention for detail and creativity to help build innovative and pertinent web applications.  My goal is to use my leadership skills and my passion for education to help others realize and reach their potential in tech.</p>  
                 </div>
